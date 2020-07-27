eed777fe474a955b01fe2a2041ec4b95669a280e
junglewilly <guillaumec.dev@gmail.com>
2020-07-27 13:40:19 +0200

    create form and fill the html and scss file

---
+++ src/form/form.js
@@ -0,0 +1,8 @@
+import "../assets/styles/styles.scss";
+import "./form.scss";
+
+const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
+let errors = [];
+
+form.addEventListener("submit", async (event) => {});
