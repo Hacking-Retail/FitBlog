4f3433fd45124b96f8a44f9e8d66a315f729d5f4
junglewilly <guillaumec.dev@gmail.com>
2020-07-27 14:19:58 +0200

    add form post method and add error

--- src/form/form.js
+++ src/form/form.js
@@ -5,4 +5,50 @@ const form = document.querySelector("form");
 const errorElement = document.querySelector("#errors");
 let errors = [];
 
-form.addEventListener("submit", async (event) => {});
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const post = Object.fromEntries(formData.entries());
+
+  if (formIsValid(post)) {
+    try {
+      const json = JSON.stringify(post);
+      const response = await fetch(
+        "https://fitnessblog-22c38.firebaseio.com/posts.json",
+        {
+          method: "POST",
+          body: json,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const body = await response.json();
+      console.log(body);
+    } catch (e) {
+      console.error("e :", e);
+    }
+  }
+});
+
+const formIsValid = (post) => {
+  if (!post.author || !post.category || !post.content) {
+    errors.push("Vous devez renseigner tous les champs ");
+  } else {
+    errors = [];
+  }
+
+  if (errors.length) {
+    let errorHTML = "";
+    errors.forEach((e) => {
+      errorHTML += `<li> ${e} </li>`;
+    });
+    errorElement.innerHTML = errorHTML;
+
+    return false;
+  } else {
+    errorElement.innerHTML = "";
+    return true;
+  }
+};
