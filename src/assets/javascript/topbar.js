61b376262443b923a4990b9a42cf9c960183c410
junglewilly <guillaumec.dev@gmail.com>
2020-07-27 16:11:43 +0200

    creating topbar responsive toggle

--- src/assets/javascript/topbar.js
+++ src/assets/javascript/topbar.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const iconMobile = document.querySelector(".header-menu-icon");
+const headerMenu = document.querySelector(".header-menu");
+
+let isMenuOpen = false;
+
+let mobileMenuDOM;
+
+const closeMenu = () => {
+  mobileMenuDOM.classList.remove("open");
+};
+
+const createMobileMenu = () => {
+  mobileMenuDOM = document.createElement("div");
+  mobileMenuDOM.classList.add("mobile-menu");
+
+  mobileMenuDOM.addEventListener("click", (event) => {
+    event.stopPropagation();
+  });
+
+  mobileMenuDOM.append(headerMenu.querySelector("ul").cloneNode(true));
+
+  headerMenu.append(mobileMenuDOM);
+};
